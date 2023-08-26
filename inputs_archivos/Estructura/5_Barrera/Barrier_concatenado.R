1adca5dde6f6ee9a08ff09cce1ec7116c9d4d279
Mateo Alvarado <mateo.alopez99@gmail.com>
2023-08-26 12:31:16 -0500

    Corrección test de barrera

--- inputs_archivos/Estructura/5_Barrera/Barrier_concatenado.R
+++ inputs_archivos/Estructura/5_Barrera/Barrier_concatenado.R
@@ -1,28 +1,32 @@
 #libraries
 library(adegenet)
 library(ape)
-#load your aligment
+
+# Cargar el alineamiento
 dna<-read.dna("concatenado_completo_VFinal_barrier.fas",format="fasta")
 gen<-DNAbin2genind(dna)
-#load the coordinates and population assignation
+
+#Cargar las coordenadas y las asignaciones por población.
 coords<-read.csv("coords_concatenado.csv",head=T)
-#Add a little noise to the points
+
+#Graficar los puntos
 coords$x<-jitter(coords$x,factor=1,amount=0.1)
 coords$y<-jitter(coords$y,factor=1,amount=0.1)
-#ADD the coordinates and pop assigment to the genind object
+
+#Añadir las coordenadas y las asignaciones por población al objeto genind de la linea 7
 gen$other$xy<-coords[,2:3]
 gen$pop<-as.factor(coords[,4])
-#choose the background graph
+
+#Establecer el fondo de la gráfica
 gab<-chooseCN(gen$other$xy,ask=F,type=1)
-#Calculate the distances
+
+#Calcular las distancias genéticas
 D<-dist(gen$tab)
-#Do all the magic, be careful with the threshold, ITS=5, COI=
+
+#Calculo del algoritmo de monmonier (linea puntuada en la gráfica indica el valor del limite que pide la consola)
 mon1 <- monmonier(gen$other$xy,D,gab)
-###iF THE THRESHOLD IS NOT SO EVIDENT, DO A PCA
-pco1 <- dudi.pco(D,scannf=FALSE,nf=1)
-D <- dist(pco1$li)
-#Re-run the monmonier
-mon1 <- monmonier(gen$other$xy,D,gab)
+
+#Graficar
 plot(mon1,add.arrows=FALSE,bwd=10,col="black")
 points(gen$other$xy, cex=2, pch=20,col=fac2col(pop(gen), col.pal=spectral))
 legend("bottomleft",leg=c("Psammolestes arthuri", "Psammolestes tertius", "Psammolestes coreodes"),pch=c(20),col=spectral(3), pt.cex=2)
